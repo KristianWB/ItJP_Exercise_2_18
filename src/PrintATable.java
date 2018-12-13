d55c2ba73f611e40cbaa7bb375e308350339e3db
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-12-13 12:28:08 +0100

    Initial commit

---
+++ src/PrintATable.java
@@ -0,0 +1,17 @@
+public class PrintATable {
+    public static void main(String[] args)   {
+
+        /* The task will be done in one step, where we display the table
+
+         */
+
+        System.out.print(
+                "a   b   pow(a, b)\n"+
+                "1   2   1\n"+
+                "2   3   8\n"+
+                "3   4   81\n"+
+                "4   5   1024\n"+
+                "5   6   15625\n"
+        );
+    }
+}
